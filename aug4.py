7c8a8bfe22aaf1e76a65faaa751942a00bad749e
bhagat-ar <bhagatarti1776@gmail.com>
2026-08-05 13:34:49 +0530

    task1: identity card
    task2: datatype
    task3: memory detect

---
+++ aug4.py
@@ -0,0 +1,40 @@
+# task 1 : identity card
+name = "ARTI BHAGAT"
+age= 22
+city="PUNE"
+dob= "20-04-2004"
+college="Keystone School of Engineering"
+Blood_Group= "B+"
+
+print("--------MY ID CARD---------")
+print("Name:", name)
+print("Age:",age)
+print("City:", city)
+print("DOB:", dob)
+print("College:", college)
+print("Blood Group: ", Blood_Group)
+print("*******************************************")
+
+
+# task2 :data types
+a= 25
+b=25.5
+c="Twenty Five "
+d= True
+e= 2+3j
+
+print("The datatype of a =", a, "is:" ,  type(a))
+print("The datatype of b =", b, "is:" ,  type(b))
+print("The datatype of c =", c, "is:" ,  type(c))
+print("The datatype of d =", d, "is:" ,  type(d))
+print("The datatype of e =", e, "is:" ,  type(e))
+print("*********************************************")
+
+# task 3: memory detect
+v1= 100
+v2= 200
+v3= 100
+
+print(id(v1))
+print(id(v2))
+print(id(v3))
